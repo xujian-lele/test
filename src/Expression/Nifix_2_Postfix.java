2da577492adc1dfa71a70fbacac5c43103f9bdd0
xujian-lele <648909477@qq.com>
2014-10-30 00:58:33 -0400

    UPCD

--- src/Expression/Nifix_2_Postfix.java
+++ src/Expression/Nifix_2_Postfix.java
@@ -1,7 +1,10 @@
 package Expression;
 
 public class Nifix_2_Postfix implements ExpressionProcessor{
-
+/**
+ * 将中缀表达式转换成后缀表达式
+ * 3 * （a + b） 
+ */
 	public String processor(String expression) {
 		return null;
 		// TODO Auto-generated method stub
