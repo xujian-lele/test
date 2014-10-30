61142e5428d5e2d0d88c84050bf2044603512726
xujian-lele <648909477@qq.com>
2014-10-29 23:14:21 -0400

    update code structure

--- src/Expression/Prefix_2_Postfix.java
+++ src/Expression/Prefix_2_Postfix.java
@@ -1,6 +1,6 @@
 package Expression;
 
-public class Nifix_2_Nifix implements ExpressionProcessor{
+public class Prefix_2_Postfix implements ExpressionProcessor{
 
 	public String processor(String expression) {
 		// TODO Auto-generated method stub
