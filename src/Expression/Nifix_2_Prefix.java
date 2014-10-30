da59c40d6159d044862db891889fceaf027d081c
xujian-lele <648909477@qq.com>
2014-10-30 05:24:57 -0400

    code refact

--- src/Expression/Nifix_2_Prefix.java
+++ src/Expression/Nifix_2_Prefix.java
@@ -4,57 +4,59 @@ import java.util.Stack;
 
 /**
  * 
- * @author xujian
- *	将中缀表达式转换成前缀表达式 :( a + b ) * (c + d)  转换成 * + a b + c d
+ * @author xujian 将中缀表达式转换成前缀表达式 :( a + b ) * (c + d) 转换成 * + a b + c d
  */
-public class Nifix_2_Prefix implements ExpressionProcessor{
+public class Nifix_2_Prefix implements ExpressionProcessor {
 	public static Nifix_2_Prefix INSTANCE = new Nifix_2_Prefix();
-	private Nifix_2_Prefix(){
-		
+
+	private Nifix_2_Prefix() {
+
 	}
 
 	public String processor(String expression) {
-		if(expression == null || expression.length() == 0) return null;
+		if (expression == null || expression.length() == 0)
+			return null;
 		Stack<String> numStack = new Stack<String>();
-	    Stack<String> optStack = new Stack<String>();
+		Stack<String> optStack = new Stack<String>();
 		String[] words = expression.split(" ");
-		for(String word : words){
+		for (String word : words) {
 			word = word.trim();
-			if(Operators.INSTANCE.isOperator(word)){
-				if(word.equalsIgnoreCase("(")){
+			if (Operators.INSTANCE.isOperator(word)) {
+				if (word.equalsIgnoreCase("(")) {
 					optStack.push(word);
-				}
-				else if(word.equalsIgnoreCase(")")){
-					while(!optStack.peek().toString().equalsIgnoreCase("(")){
+				} else if (word.equalsIgnoreCase(")")) {
+					while (!optStack.peek().toString().equalsIgnoreCase("(")) {
 						String str = popStack(numStack, optStack);
 					}
 					optStack.pop();
-				}else{
+				} else {
 					int wordPriority = getPriority(word);
-					int optPriority ;
-					if(optStack.isEmpty()){
+					int optPriority;
+					if (optStack.isEmpty()) {
 						optPriority = -1;
-					}else{
+					} else {
 						optPriority = getPriority(optStack.peek().toString());
 					}
-					
-					if(wordPriority >= optPriority){
+
+					if (wordPriority >= optPriority) {
 						optStack.push(word);
-					}else{
-						//输入符优先级别低
-						while(!optStack.isEmpty() && wordPriority < getPriority(optStack.peek().toString())){
+					} else {
+						// 输入符优先级别低
+						while (!optStack.isEmpty()
+								&& wordPriority < getPriority(optStack.peek()
+										.toString())) {
 							popStack(numStack, optStack);
 						}
 						optStack.push(word);
 					}
 				}
-				
-			}else{
+
+			} else {
 				numStack.push(word);
 			}
 		}
-		
-		while(!optStack.isEmpty()){
+
+		while (!optStack.isEmpty()) {
 			popStack(numStack, optStack);
 		}
 		return numStack.pop();
@@ -62,15 +64,18 @@ public class Nifix_2_Prefix implements ExpressionProcessor{
 
 	/**
 	 * 
-	 * @param numStack	数据栈
-	 * @param optStack	操符栈
-	 * function ： 从数据栈中弹出最顶端的两个元素b，a；从操作符栈中淡出最顶端的那个元素o；将oab 加入到数据栈中。
+	 * @param numStack
+	 *            数据栈
+	 * @param optStack
+	 *            操符栈 function ： 从数据栈中弹出最顶端的两个元素b，a；从操作符栈中淡出最顶端的那个元素o；将oab
+	 *            加入到数据栈中。
 	 */
 	private String popStack(Stack<String> numStack, Stack<String> optStack) {
-		if(numStack.isEmpty() || numStack.size()==1) return "";
-		if(optStack.isEmpty()){
+		if (numStack.isEmpty() || numStack.size() == 1)
+			return "";
+		if (optStack.isEmpty()) {
 			StringBuffer sb = new StringBuffer();
-			while (!numStack.isEmpty() && numStack.size() >1) {
+			while (!numStack.isEmpty() && numStack.size() > 1) {
 				StringBuffer sb1 = new StringBuffer();
 				sb1.append(numStack.pop());
 				StringBuffer sb2 = new StringBuffer();
@@ -80,18 +85,18 @@ public class Nifix_2_Prefix implements ExpressionProcessor{
 			}
 			return numStack.pop();
 		}
-		
-		//俩栈都非空
+
+		// 俩栈都非空
 		StringBuffer sb1 = new StringBuffer();
 		sb1.append(numStack.pop());
 		StringBuffer sb2 = new StringBuffer();
 		sb2.append(numStack.pop());
 		String result = sb2 + " " + sb1;
-		
-		numStack.push(optStack.pop()+" "+result);
+
+		numStack.push(optStack.pop() + " " + result);
 		return numStack.peek();
 	}
-	
+
 	public int getPriority(String s) {
 		return Operators.INSTANCE.optors.get(s);
 	}
